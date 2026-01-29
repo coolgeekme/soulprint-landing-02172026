e4b2db6f054579b1068e5bd82d35e77ac77b02e9
Defy <defy@clawd.bot>
2026-01-28 20:57:05 -0600

    fix: add dark class to html for tailwind dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
